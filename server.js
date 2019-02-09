3f52e0f6a14a9f02093dfb04bfb347ff874c3662
srdoyen <srdoyen@gmail.com>
2019-02-08 23:00:29 -0800

    Register users via API

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //db config
 const db = require("./config/keys").mongoURI;
